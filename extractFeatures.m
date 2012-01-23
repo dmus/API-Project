0e6b7a90fc68e2a79b6d45deede06de4e6ad484f
dmus <derkmus@gmail.com>
2012-01-23 19:57:12 +0100

    added README

--- extractFeatures.m
+++
@@ -1,9 +0,0 @@
-function [f] = extractFeatures(model, mu, range, filename)
-%EXTRACTFEATURES Summary of this function goes here
-%   Detailed explanation goes here
-
-f = preprocess(filename);
-    
-
-f = rbmVtoH(model, f);
-end
--- preprocess.m
+++
@@ -1,7 +0,0 @@
-function [F] = preprocess(filename)
-%PREPROCESS Apply rfft and do some dimension reduction
-%   Detailed explanation goes here
-    
-
-end
-
